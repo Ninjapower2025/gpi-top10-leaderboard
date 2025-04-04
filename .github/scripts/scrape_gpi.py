b436947b5315dc32cc947864d5746b8b4028b38d
Ninjapower2025 <mpsraymond@gmail.com>
2025-04-04 16:00:24 +0800

    Update scrape_gpi.py

--- .github/scripts/scrape_gpi.py
+++ .github/scripts/scrape_gpi.py
@@ -32,7 +32,7 @@ def scrape_top30():
             except:
                 continue
 
-        with open("top30_final.json", "w", encoding="utf-8") as f:
+        with open("top30.json", "w", encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent=2)
 
         browser.close()
